02afba0a3a86917921bb0ffccb6c6534a60f11c3
Dom Rodriguez <shymega@shymega.org.uk>
2023-10-11 23:19:24 +0100

    refactor(network): Adjust some NM settings
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/shared/linux/networking.nix
+++ hosts/shared/linux/networking.nix
@@ -1,16 +1,26 @@
 { config, ... }:
 {
-  networking.networkmanager.enable = true;
-  networking.networkmanager.dispatcherScripts = [
-    {
-      source = "/persist/etc/NetworkManager/dispatcher.d/05-wifi-toggle";
-      type = "basic";
-    }
-    {
-      source = "/persist/etc/NetworkManager/dispatcher.d/10-net-targets";
-      type = "basic";
-    }
-  ];
+  networkmanager = {
+    extraConfig = ''
+      [connectivity]
+      uri=http://www.archlinux.org/check_network_status.txt
+      interval=0
+    '';
+    dns = "systemd-resolved";
+    wifi.macAddress = "stable";
+    wifi.powersave = false;
+    enable = true;
+    dispatcherScripts = [
+      {
+        source = "/persist/etc/NetworkManager/dispatcher.d/05-wifi-toggle";
+        type = "basic";
+      }
+      {
+        source = "/persist/etc/NetworkManager/dispatcher.d/10-net-targets";
+        type = "basic";
+      }
+    ];
+  };
 
   programs.nm-applet = {
     enable = true;
