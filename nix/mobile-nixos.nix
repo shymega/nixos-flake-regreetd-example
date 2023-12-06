6ee7f5cc90a8580027081a0303a50b2edc4e99ef
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-06 18:20:26 +0000

    fix(mobile-nix): Fix home-manager and missing modules
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- nix/mobile-nixos.nix
+++ nix/mobile-nixos.nix
@@ -23,6 +23,14 @@ let
         };
       };
       modules = [
+        inputs.agenix.nixosModules.default
+        inputs.nix-ld.nixosModules.nix-ld
+        inputs.nix-index-database.nixosModules.nix-index
+        {
+          environment.systemPackages = [
+            inputs.agenix.packages.${system}.default
+          ];
+        }
         ../secrets
         (../hosts/android/mobile-nixos + "/${hostname}")
       ] ++ baseModules ++ homeModules ++ extraModules;
