c64c84c3a24bf97969b815f7057eb4d2427f6edc
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-12 15:36:43 +0000

    fix(xdg): Hopefully fix XDG portals
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/xdg.nix
+++ common/nixos/xdg.nix
@@ -5,7 +5,9 @@
 { pkgs, ... }: {
   xdg.portal = {
     enable = true;
+    config.common.default = "*";
     wlr.enable = true;
+    xdgOpenUsePortal = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-kde ];
   };
 }
