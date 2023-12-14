930158abf37901eaa3e53fc89f5b007050c5346d
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-14 19:05:19 +0000

    refactor(sway): Refactor Sway config
    NOTE: Only KDE and GNOME seem to work with screen sharing windows.
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/wayland.nix
+++ common/nixos/wayland.nix
@@ -3,6 +3,35 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 { config, pkgs, lib, ... }:
+let
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text =
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in
+      ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        gnome_schema=org.gnome.desktop.interface
+        gsettings set $gnome_schema gtk-theme 'Dracula'
+      '';
+  };
+in
 {
   programs.sway = {
     enable = true;
@@ -11,26 +40,37 @@
     extraPackages = with pkgs; [
       alacritty
       clipman
+      configure-gtk
+      dbus-sway-environment
+      dracula-theme
+      gnome3.adwaita-icon-theme
       grim
       kanshi
       mako
+      slurp
       slurp
       sway-contrib.grimshot
       swayidle
       swaylock
       waybar
+      waybar
+      wayland
       wdisplays
       wf-recorder
       wl-clipboard
       wofi
-      waybar
+      xdg-utils
     ];
     extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      export _JAVA_AWT_WM_NONREPARENTING=1
       export MOZ_ENABLE_WAYLAND=1
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      export SDL_VIDEODRIVER=wayland
+      export SSH_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      export SUDO_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      export XDG_CURRENT_DESKTOP=sway
+      export XDG_SESSION_TYPE=wayland
+      export _JAVA_AWT_WM_NONREPARENTING=1
     '';
   };
 }
