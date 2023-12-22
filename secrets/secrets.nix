40f1837a2955e329d04c08bb4e52390620158a9a
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-22 22:16:48 +0000

    chore(secrets): Reencrypt secrets
    TODO: rebase.
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,12 +3,18 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 let
-  dzrodriguez-NEO = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF1L44C2mZnuRysLRq98P+ri7pkpl0dyTzr/3EAQ7Qov dzrodriguez@NEO-LINUX";
-  users = [ dzrodriguez-NEO ];
+  dzrodriguez-NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF1L44C2mZnuRysLRq98P+ri7pkpl0dyTzr/3EAQ7Qov dzrodriguez@NEO-LINUX";
+  users = [ dzrodriguez-NEO-LINUX ];
 
   NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8Stawqd09idKurIZ+eSSEbmWdXIlQQJ4eaMo6bmClv";
   TRINITY-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7DcpK52k1LV7EIuPTw47irtNFn2TfoQVtd860tVpvp";
-  systems = [ NEO-LINUX TRINITY-LINUX ];
+  DELTA-ZERO = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOBP4prVx3gdi5YMW4dzy06s46aobpyY8IlFBDVgjDU root@DELTA-ZERO";
+
+  personal = [ NEO-LINUX TRINITY-LINUX ];
+  work = [ ];
+  rnet-servers = [ DELTA-ZERO ];
+
+  systems = personal ++ work ++ rnet-servers;
 
   allKeys = users ++ systems;
 in
@@ -16,8 +22,4 @@ in
   "postfix_sasl_passwd.age".publicKeys = allKeys;
   "postfix_sender_relay.age".publicKeys = allKeys;
   "user_dzrodriguez.age".publicKeys = allKeys;
-  "taskwarrior_sync_cert.age".publicKeys = allKeys;
-  "taskwarrior_sync_key.age".publicKeys = allKeys;
-  "taskwarrior_sync_ca.age".publicKeys = allKeys;
-  "taskwarrior_sync_cred.age".publicKeys = allKeys;
 }
