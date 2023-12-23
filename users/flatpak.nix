6228fe90eb2c432945e98b0f6d7bad020a1a4dec
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-23 00:23:51 +0000

    chore(nix-flatpak): remove import
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- users/flatpak.nix
+++ users/flatpak.nix
@@ -3,8 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 { inputs, ... }: {
-  imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
-
   services.flatpak = {
     enable = true;
     packages = [
