30f62b636791a5d71e80305d03beb8ff218fff7e
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-28 20:01:09 +0000

    fix(fonts): Trim down fonts
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/core/fonts.nix
+++ common/core/fonts.nix
@@ -2,24 +2,13 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-{ config, lib, inputs, outputs, pkgs, ... }: {
+{ pkgs, ... }: {
   fonts.packages = with pkgs; [
-    open-dyslexic
-    fira
-    fira-code
-    jetbrains-mono
-    font-awesome_5
-    font-awesome_4
-    noto-fonts
-    noto-fonts-emoji
-    emojione
-    twemoji-color-font
-    ibm-plex
-    source-code-pro
-    jetbrains-mono
-    font-awesome
     corefonts
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    ibm-plex
+    jetbrains-mono
+    jetbrains-mono
+    source-code-pro
   ];
 
   fonts.fontDir.enable = true;
