37f9dabce9f749987e523ba39ec1e79acfa0c97b
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-29 15:46:36 +0000

    chore(tmux): Disable mouse
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/core/tmux.nix
+++ common/core/tmux.nix
@@ -17,13 +17,7 @@ in
     keyMode = "emacs";
     secureSocket = false; # Force tmux to use /tmp for sockets (WSL2 compat)
 
-    plugins = with pkgs.tmuxPlugins; [
-      better-mouse-mode
-    ];
-
     extraConfig = ''
-      # Mouse works as expected
-      set-option -g mouse on
       # easy-to-remember split pane commands
       bind | split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
