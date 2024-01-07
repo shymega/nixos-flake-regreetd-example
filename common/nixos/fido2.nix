643a111d8fd7cf36220fed760a61d7511dd9feca
Dom Rodriguez <shymega@shymega.org.uk>
2024-01-07 20:59:22 +0000

    feat(yubikey): Add U2F signin
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/fido2.nix
+++ common/nixos/fido2.nix
@@ -4,11 +4,27 @@
 
 { config, pkgs, ... }:
 {
-  security.pam.u2f = {
-    enable = true;
-    cue = true;
+  security.pam = {
+    services = {
+      login.u2fAuth = true;
+    };
+
+    u2f = {
+      enable = true;
+      cue = true;
+    };
   };
-  services.udev.packages = with pkgs; [
-    solo2-cli
-  ];
-}
+
+  services.pcscd.enable = true;
+
+  services.udev = {
+    packages = with pkgs; [ yubikey-personalization solo2-cli ];
+    extraRules = ''
+      ACTION=="remove",\
+       ENV{ID_BUS}=="usb",\
+       ENV{ID_MODEL_ID}=="0407",\
+       ENV{ID_VENDOR_ID}=="1050",\
+       ENV{ID_VENDOR}=="Yubico",\
+       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+    '';
+  }
