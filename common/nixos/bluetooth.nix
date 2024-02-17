d0b82cc247d6d5738f84f13226ce097c732d32e1
Dom Rodriguez <shymega@shymega.org.uk>
2024-02-17 19:35:02 +0000

    chore: Tidy up attrs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/bluetooth.nix
+++ common/nixos/bluetooth.nix
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-{ config, ... }: {
+{ ... }: {
   hardware.bluetooth.enable = true;
   hardware.bluetooth.settings = {
     General = { Enable = "Source,Sink,Media,Socket"; };
