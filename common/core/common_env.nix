bf774a9b0b94f17dc434d9635600282f60dc7ad4
Dom Rodriguez <shymega@shymega.org.uk>
2024-02-17 19:35:02 +0000

    chore: Tidy up attrs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/core/common_env.nix
+++ common/core/common_env.nix
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-{ inputs, outputs, pkgs, ... }: {
+{ pkgs, ... }: {
   environment = {
     variables = {
       TERMINAL = "${pkgs.alacritty}/bin/alacritty";
