d540f6e3dfdd1e6ebfb4988aede109c993bd574b
Dom Rodriguez <shymega@shymega.org.uk>
2024-03-09 00:00:19 +0000

    fix: Fix broken Nix files
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/kernel_params.nix
+++ common/nixos/kernel_params.nix
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-{ lib }:
+{ lib, ... }:
 {
   boot.kernelParams = lib.mkAfter [
     "loglevel=3"
--- common/nixos/sound.nix
+++ common/nixos/sound.nix
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-{ lib }: {
+{ lib, ... }: {
   hardware.pulseaudio.enable = lib.mkForce false;
 
   sound = {
