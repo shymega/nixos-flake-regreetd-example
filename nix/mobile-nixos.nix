407ff133e9a36006748bb4657b5de5d86f08e30c
Dom Rodriguez <shymega@shymega.org.uk>
2024-03-15 22:11:54 +0000

    feat(mobile-nixos): Populate Mobile NixOS module
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- nix/mobile-nixos.nix
+++ nix/mobile-nixos.nix
@@ -2,4 +2,57 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-{ self, inputs, pkgs }: { }
+{ self, inputs, pkgs }:
+let
+mkMobileNixosConfig =
+    { system ? "aarch64-linux"
+    , hostname ? "UNDEFINED-HOSTNAME"
+    , nixpkgs ? inputs.nixpkgs
+    , baseModules ? [
+        ../common/android/mobile-nixos
+      ]
+    , homeModules ? [
+        mkHomeManagerConfig
+        { }
+      ]
+    , extraModules ? [ ]
+    }: inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = builtins.attrValues self.overlays;
+        config = {
+          allowUnfree = true;
+          allowBroken = false;
+          allowInsecure = false;
+          allowUnsupportedSystem = false;
+        };
+      };
+      home-manager-path = inputs.home-manager.outPath;
+      modules = [
+        inputs.agenix.nixosModules.default
+        ../secrets
+        (../hosts/android/nix-on-droid + "/${hostname}")
+      ] ++ baseModules ++ homeModules ++ extraModules;
+      extraSpecialArgs = { inherit self inputs nixpkgs; };
+    };
+
+  mkHomeManagerConfig =
+    { usePkgs ? true
+    , extraModules ? [
+      ]
+    , specialArgs ? [{ inherit self inputs; }]
+    }:
+    inputs.home-manager.nixosModules.home-manager {
+      home-manager = {
+        useGlobalPkgs = usePkgs;
+        useUserPackages = usePkgs;
+        sharedModules = [
+          inputs.agenix.homeManagerModules.default
+        ] ++ extraModules;
+        extraSpecialArgs = specialArgs;
+      };
+    };
+in
+{
+
+}
