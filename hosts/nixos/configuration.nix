94b14c5851a92d8b8fd26af7c3f7351e0495bc12
Dom Rodriguez <shymega@shymega.org.uk>
2024-04-25 13:50:08 +0100

    fix: Test fix for DNS resolution with CF
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -78,9 +78,10 @@
       dnssec = "allow-downgrade";
       fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
       extraConfig = ''
-        DNS=1.1.1.1#1dot1dot1dot1.cloudflare-dns.com 1.0.0.1#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1111#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1001#1dot1dot1dot1.cloudflare-dns.com
-        DNSOverTLS=opportunistic
+        	DNS=8.8.8.8 8.8.4.4
+          DNSOverTLS=opportunistic
       '';
+      #        DNS=1.1.1.1#1dot1dot1dot1.cloudflare-dns.com 1.0.0.1#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1111#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1001#1dot1dot1dot1.cloudflare-dns.com
     };
   };
 
