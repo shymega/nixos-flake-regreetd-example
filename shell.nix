91578a45495869ace7dec549ffb521b07f202a90
Dom Rodriguez <shymega@shymega.org.uk>
2024-05-03 00:28:12 +0100

    refactor: Refactor `shell.nix`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- shell.nix
+++ shell.nix
@@ -1,29 +1,13 @@
 # SPDX-FileCopyrightText: 2023 Dom Rodriguez <shymega@shymega.org.uk>
 #
 # SPDX-License-Identifier: GPL-3.0-only
-
-# Shell for bootstrapping flake-enabled nix and other tooling
-{ pkgs ? # If pkgs is not defined, instanciate nixpkgs from locked commit
-  let
-    lock =
-      (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
-    nixpkgs = fetchTarball {
-      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
-      sha256 = lock.narHash;
-    };
-  in
-  import nixpkgs { overlays = [ ]; }
-, ...
-}:
-pkgs.mkShell {
-  NIX_CONFIG =
-    "extra-experimental-features = nix-command flakes repl-flake";
-  nativeBuildInputs = with pkgs; [
-    nix
-    home-manager
-    git
-
-    gnupg
-    age
-  ];
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
