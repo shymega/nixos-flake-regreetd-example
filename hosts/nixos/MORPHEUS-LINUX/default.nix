72ada2594b10e07f79ee0364d41e793d4407693e
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-08 23:18:48 +0100

    feat: Add MORPHEUS-LINUX
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/MORPHEUS-LINUX/default.nix
@@ -0,0 +1,143 @@
+# SPDX-FileCopyrightText: 2023 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{ config, pkgs, ... }:
+let
+  enableXanmod = false;
+in
+{
+  environment.etc."crypttab".text = ''
+    homecrypt /dev/disk/by-label/HOMECRYPT /persist/etc/.homecrypt.bin
+  '';
+  networking.hostName = "MORPHEUS-LINUX";
+  networking.hostId = "9f3120c5";
+  time.timeZone = "Europe/London";
+
+  boot = {
+    supportedFilesystems = [ "ntfs" "zfs" ];
+
+    kernelPackages =
+      if enableXanmod then
+        pkgs.linuxPackages_xanmod_latest
+      else
+        config.boot.zfs.package.latestCompatibleLinuxPackages;
+
+    extraModulePackages = with config.boot.kernelPackages; [ zfs ];
+
+    extraModprobeConfig = ''
+      options kvm_amd nested=1
+      options kvm ignore_msrs=1 report_ignored_msrs=0
+    '';
+
+    zfs.devNodes = "/dev/MORPHEUS-LINUX/ROOT";
+
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "819200";
+      "kernel.printk" = "3 3 3 3";
+    };
+
+    initrd.luks.devices = {
+      nixos = {
+        device = "/dev/disk/by-label/NIXOS_PART";
+        preLVM = true;
+        allowDiscards = true;
+      };
+    };
+
+    plymouth = {
+      enable = false;
+    };
+
+    loader = {
+      systemd-boot = {
+        enable = false;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        device = "nodev";
+        efiSupport = true;
+        default = "saved";
+        enable = true;
+        useOSProber = true;
+      };
+      timeout = 6;
+    };
+
+    initrd.systemd.services.rollback = {
+      description = "Rollback ZFS datasets to a pristine state";
+      wantedBy = [
+        "initrd.target"
+      ];
+      after = [
+        "zfs-import-tank.service"
+      ];
+      before = [
+        "sysroot.mount"
+      ];
+      path = with pkgs; [
+        zfs
+      ];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      script = ''
+        zfs rollback -r tank/local/root@blank
+      '';
+    };
+  };
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "powersave";
+  };
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = false;
+      extraPackages = pkgs.lib.mkForce (with pkgs; [
+        amdvlk
+      ]);
+      extraPackages32 = with pkgs; [
+        driversi686Linux.amdvlk
+      ];
+    };
+  };
+
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    package = pkgs.ollama;
+    environmentVariables = {
+      HSA_OVERRIDE_GFX_VERSION = "11.0.0"; # 780M
+    };
+  };
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  services = {
+    zfs = {
+      trim = {
+        enable = true;
+        interval = "Sat *-*-* 04:00:00";
+      };
+      autoScrub = {
+        enable = true;
+        interval = "Sat *-*-* 05:00:00";
+      };
+    };
+    xserver = {
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
+    };
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = false;
+  };
+}
