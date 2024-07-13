ec2d44506e460f1f42602a37f40642848ebe5362
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 15:00:30 +0100

    refactor: Move disks config - MORPHEUS-LINUX
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/MORPHEUS-LINUX/hardware-configuration.nix
+++ hosts/nixos/MORPHEUS-LINUX/hardware-configuration.nix
@@ -4,6 +4,7 @@
 
 { config, lib, ... }:
 {
+  imports = [ ./disks.nix ];
   boot = {
     initrd.availableKernelModules = [
       "nvme"
@@ -17,77 +18,7 @@
     initrd.kernelModules = [ "dm-snapshot" "amdgpu" ];
     kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
-    resumeDevice = "/dev/disk/by-label/NIXOS_SWAP";
   };
-
-  fileSystems = {
-    "/" =
-      {
-        device = "tank/local/root";
-        fsType = "zfs";
-      };
-    "/nix" =
-      {
-        device = "tank/local/nixos-store";
-        neededForBoot = true;
-        fsType = "zfs";
-      };
-    "/persist" =
-      {
-        device = "tank/safe/persist";
-        neededForBoot = true;
-        fsType = "zfs";
-      };
-    "/var" =
-      {
-        device = "tank/safe/var-store";
-        neededForBoot = true;
-        fsType = "zfs";
-      };
-    "/etc/nixos" =
-      {
-        device = "tank/safe/nixos-config";
-        neededForBoot = true;
-        fsType = "zfs";
-      };
-    "/boot/efi/NIXOS" = {
-      device = "/dev/disk/by-label/ESP_NIXOS"; # Use Refind on /dev/disk/by-label/ESP_PRIMARY
-      neededForBoot = true;
-      fsType = "vfat";
-    };
-    "/boot/efi/PRIMARY" = {
-      device = "/dev/disk/by-label/ESP_PRIMARY";
-      neededForBoot = true;
-      options = [ "ro" "nofail" ];
-      fsType = "vfat";
-    };
-    "/boot/efi/WINNT" = {
-      device = "/dev/disk/by-label/ESP_WINNT";
-      options = [ "ro" "nofail" ];
-      fsType = "vfat";
-    };
-    "/boot/efi/BAZZITE" = {
-      device = "/dev/disk/by-label/ESP_BAZZITE";
-      options = [ "ro" "nofail" ];
-      fsType = "vfat";
-    };
-    "/home" = {
-      device = "/dev/disk/by-label/HOME";
-      fsType = "xfs";
-      options = [ "defaults" "noatime" "ssd" ];
-    };
-    "/data/Games" = {
-      device = "/dev/disk/by-label/GAMES";
-      fsType = "btrfs";
-      options = [ "defaults" "noatime" "ssd" ];
-    };
-    "/data/VMs" = {
-      device = "/dev/disk/by-label/VIRSTOR";
-      fsType = "btrfs";
-      options = [ "defaults" "noatime" "ssd" ];
-    };
-  };
-  swapDevices = [{ device = "/dev/disk/by-label/NIXOS_SWAP"; }];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode =
