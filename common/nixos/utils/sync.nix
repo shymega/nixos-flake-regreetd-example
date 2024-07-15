dc6fa9cdce1c29841d24961f479bdc913840163d
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-15 22:47:51 +0100

    fix: Fix package def
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/utils/sync.nix
+++ common/nixos/utils/sync.nix
@@ -1,15 +1,14 @@
 { pkgs, ... }:
 {
-  tidy-syncthing = pkgs.writeScriptBin "clean-syncthing" ''
-    #! ${pkgs.stdenv.shell}
-    set -eu
+  environment.systemPackages = [
+    (pkgs.writeScriptBin "clean-syncthing" ''
+      #! ${pkgs.stdenv.shell}
+      set -eu
 
-    ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname "*sync-conflict*" -print -delete
-    ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname ".#*" -print -delete
-    ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname "*~*" -print -delete
-    ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname ".syncthing*" -print -delete
-  '';
-
-  
-
+      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname "*sync-conflict*" -print -delete
+      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname ".#*" -print -delete
+      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname "*~*" -print -delete
+      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname ".syncthing*" -print -delete
+    '')
+  ];
 }
