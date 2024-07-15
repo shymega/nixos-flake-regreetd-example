beced9a8a6e33405f9f2f71c47fba6a9aaa42760
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-15 19:02:35 +0100

    fix: Fix dbus-update-activation-environment
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/wayland.nix
+++ common/nixos/wayland.nix
@@ -10,7 +10,7 @@ let
     executable = true;
 
     text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway DISPLAY PATH XDG_SESSION_TYPE
       systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
       systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
     '';
