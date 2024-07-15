c0a5b1d4bd7acae08c0708c90aaf34b308fb3a4f
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-15 15:23:14 +0100

    fix: Fix typo in OfflineIMAP overlay
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- nix/overlays/stable/offlineimap.nix
+++ nix/overlays/stable/offlineimap.nix
@@ -6,7 +6,7 @@ final: prev: {
   offlineimap = prev.offlineimap.overrideAttrs {
     version = "git";
     src = builtins.fetchGit {
-      url = "https://github.com/shymega/OfflineIMAP";
+      url = "https://github.com/shymega/OfflineIMAP3";
       ref = "shymega-fixes";
       rev = "9e5cdba6e75f6c69c41b8d9880bd7defc74708a4";
     };
