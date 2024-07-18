84abe32907522cd11d81d4613e517be9f9ca404b
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-18 21:31:00 +0100

    chore: Run `nixpkgs-fmt`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/offlineimap.nix
+++ overlays/stable/offlineimap.nix
@@ -1,9 +1,9 @@
 final: prev: {
-              offlineimap-shymega = prev.offlineimap.overrideAttrs (finalAttrs: prevAttrs: {
-                version = "git";
-                src = builtins.fetchGit {
-                  url = "https://github.com/shymega/OfflineIMAP3";
-                  ref = "shymega-fixes";
-                };
-              });
-            }
+  offlineimap-shymega = prev.offlineimap.overrideAttrs (finalAttrs: prevAttrs: {
+    version = "git";
+    src = builtins.fetchGit {
+      url = "https://github.com/shymega/OfflineIMAP3";
+      ref = "shymega-fixes";
+    };
+  });
+}
--- pkgs/dwl/dwl.nix
+++ pkgs/dwl/dwl.nix
@@ -1,18 +1,18 @@
-            final: prev: {
-              dwl = pkgs.dwl.overrideAttrs
-                (finalAttrs: {
-                  src = inputs.dwl-source;
-                  patches = [
-                    ./pkgs/dwl/dwl-patches/attachbottom.patch
-                    ./pkgs/dwl/dwl-patches/autostart.patch
-                    ./pkgs/dwl/dwl-patches/focusdirection.patch
-                    ./pkgs/dwl/dwl-patches/monfig.patch
-                    ./pkgs/dwl/dwl-patches/point.patch
-                    ./pkgs/dwl/dwl-patches/restoreTiling.patch
-                    ./pkgs/dwl/dwl-patches/save_monitor_state.patch
-                    ./pkgs/dwl/dwl-patches/steam_fix.patch
-                    ./pkgs/dwl/dwl-patches/toggleKbLayout.patch
-                    ./pkgs/dwl/dwl-patches/vanitygaps.patch
-                  ];
-                });
-            }
+final: prev: {
+  dwl = pkgs.dwl.overrideAttrs
+    (finalAttrs: {
+      src = inputs.dwl-source;
+      patches = [
+        ./pkgs/dwl/dwl-patches/attachbottom.patch
+        ./pkgs/dwl/dwl-patches/autostart.patch
+        ./pkgs/dwl/dwl-patches/focusdirection.patch
+        ./pkgs/dwl/dwl-patches/monfig.patch
+        ./pkgs/dwl/dwl-patches/point.patch
+        ./pkgs/dwl/dwl-patches/restoreTiling.patch
+        ./pkgs/dwl/dwl-patches/save_monitor_state.patch
+        ./pkgs/dwl/dwl-patches/steam_fix.patch
+        ./pkgs/dwl/dwl-patches/toggleKbLayout.patch
+        ./pkgs/dwl/dwl-patches/vanitygaps.patch
+      ];
+    });
+}
