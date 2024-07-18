218a32a87e3cbac19c7dc64d0169ae0491a3e34c
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-18 21:31:21 +0100

    fix: Fix XDG portals
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/xdg.nix
+++ common/nixos/xdg.nix
@@ -5,7 +5,7 @@
 { pkgs, ... }: {
   xdg.portal = {
     enable = true;
-    config.common.default = "gtk";
+    config.common.default = "wlr;gtk";
     wlr.enable = true;
     xdgOpenUsePortal = true;
   };
