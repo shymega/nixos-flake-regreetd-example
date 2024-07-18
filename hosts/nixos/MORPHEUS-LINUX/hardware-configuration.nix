8bfeefefc5ce9398b8cff96e420ab484c4db7920
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-18 23:01:29 +0100

    chore: Fix lints and run formatter
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/MORPHEUS-LINUX/hardware-configuration.nix
+++ hosts/nixos/MORPHEUS-LINUX/hardware-configuration.nix
@@ -5,10 +5,14 @@
 { config, lib, ... }:
 {
   imports = [ ./disks.nix ];
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "sdhci_pci" "hid_apple" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "i2c_dev" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "sdhci_pci" "hid_apple" ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-amd" "i2c_dev" ];
+    extraModulePackages = [ ];
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode =
--- hosts/nixos/NEO-LINUX/default.nix
+++ hosts/nixos/NEO-LINUX/default.nix
@@ -15,7 +15,37 @@ in
 
   boot = {
     supportedFilesystems = [ "ntfs" "zfs" ];
-    initrd.supportedFilesystems = [ "ntfs" "zfs" ];
+    initrd = {
+      supportedFilesystems = [ "ntfs" "zfs" ];
+
+      luks.devices = {
+        nixos = {
+          device = "/dev/disk/by-label/NIXOS";
+          preLVM = true;
+          allowDiscards = true;
+        };
+      };
+      systemd.services.rollback = {
+        description = "Rollback ZFS datasets to a pristine state";
+        wantedBy = [
+          "initrd.target"
+        ];
+        after = [
+          "zfs-import-tank.service"
+        ];
+        before = [
+          "sysroot.mount"
+        ];
+        path = with pkgs; [
+          zfs
+        ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          zfs rollback -r tank/local/root@blank
+        '';
+      };
+    };
 
     kernelPackages =
       if enableXanmod then
@@ -35,14 +65,6 @@ in
     kernel.sysctl = {
       "fs.inotify.max_user_watches" = "819200";
       "kernel.printk" = "3 3 3 3";
-    };
-
-    initrd.luks.devices = {
-      nixos = {
-        device = "/dev/disk/by-label/NIXOS";
-        preLVM = true;
-        allowDiscards = true;
-      };
     };
 
     plymouth = {
@@ -65,27 +87,6 @@ in
         useOSProber = true;
       };
       timeout = 6;
-    };
-
-    initrd.systemd.services.rollback = {
-      description = "Rollback ZFS datasets to a pristine state";
-      wantedBy = [
-        "initrd.target"
-      ];
-      after = [
-        "zfs-import-tank.service"
-      ];
-      before = [
-        "sysroot.mount"
-      ];
-      path = with pkgs; [
-        zfs
-      ];
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig.Type = "oneshot";
-      script = ''
-        zfs rollback -r tank/local/root@blank
-      '';
     };
   };
 
