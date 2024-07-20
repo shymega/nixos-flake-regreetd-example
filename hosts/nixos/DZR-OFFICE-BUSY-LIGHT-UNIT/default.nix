b80877a014f313d8a570ed326d8ca3698d4518a2
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-20 14:59:01 +0100

    feat: Add some Pi Zero units
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/DZR-OFFICE-BUSY-LIGHT-UNIT/default.nix
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2023 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{ inputs, config, pkgs, lib, ... }:
+{
+  nixpkgs.crossSystem = lib.systems.elaborate lib.systems.examples.raspberryPi;
+
+  networking = {
+    hostName = "DZR-OFFICE-BUSY-LIGHT-UNIT";
+  };
+  nixpkgs.hostPlatform = "armv6l-linux";
+
+  environment.systemPackages = with pkgs; [
+    tmux
+    vim
+    libraspberrypi
+    raspberrypi-eeprom
+    nixpkgs-fmt
+  ];
+
+  system.stateVersion = "24.05";
+
+  sdImage = {
+    compressImage = false;
+    imageName = "DZR-OFFICE-BUSY-LIGHT-UNIT.img";
+
+    populateRootCommands = "";
+    populateFirmwareCommands = with config.system.build; ''
+      ${installBootLoader} ${toplevel} -d ./firmware
+    '';
+    firmwareSize = 64;
+  };
+
+  hardware = {
+    # needed for wlan0 to work (https://github.com/NixOS/nixpkgs/issues/115652)
+    enableRedistributableFirmware = pkgs.lib.mkForce false;
+    firmware = with pkgs; [
+      raspberrypiWirelessFirmware
+    ];
+  };
+
+  networking = {
+    interfaces."wlan0".useDHCP = true;
+    wireless = {
+      enable = true;
+      interfaces = [ "wlan0" ];
+      networks = {
+        "RNET" = {
+          psk = lib.strings.removeSuffix "\n" (builtins.readFile config.age.secrets.home_network_iot_p.path);
+        };
+      };
+    };
+  };
+}
