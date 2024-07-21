ec44328fc4cf6bae8642bd73c8bc43dd3e98c9d3
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-21 23:34:38 +0100

    feat: Add more WSL hosts
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- modules/wsl.nix
+++ modules/wsl.nix
@@ -65,5 +65,51 @@ in
     ];
   };
 
+  NEO-WSL = mkNixosWslConfig rec {
+    hostname = "NEO-WSL";
+    system = "x86_64-linux";
+    extraModules = [
+      ({ lib, ... }: {
+        # Backward-compat for 24.05, can be removed after we drop 24.05 support
+        imports = lib.optionals (lib.versionOlder lib.version "24.11pre") [
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "extraPackages32" ] [ "hardware" "opengl" "extraPackages32" ])
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "enable32Bit" ] [ "hardware" "opengl" "driSupport32Bit" ])
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "package" ] [ "hardware" "opengl" "package" ])
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "package32" ] [ "hardware" "opengl" "package32" ])
+        ];
+      })
+      {
+        environment.systemPackages = [
+          inputs.agenix.packages.${system}.default
+          inputs.nix-alien.packages.${system}.nix-alien
+        ];
+      }
+      ../hosts/nixos/configuration.nix
+    ];
+  };
+
+  TWINS-WSL = mkNixosWslConfig rec {
+    hostname = "TWINS-WSL";
+    system = "x86_64-linux";
+    extraModules = [
+      ({ lib, ... }: {
+        # Backward-compat for 24.05, can be removed after we drop 24.05 support
+        imports = lib.optionals (lib.versionOlder lib.version "24.11pre") [
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "extraPackages32" ] [ "hardware" "opengl" "extraPackages32" ])
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "enable32Bit" ] [ "hardware" "opengl" "driSupport32Bit" ])
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "package" ] [ "hardware" "opengl" "package" ])
+          (lib.mkAliasOptionModule [ "hardware" "graphics" "package32" ] [ "hardware" "opengl" "package32" ])
+        ];
+      })
+      {
+        environment.systemPackages = [
+          inputs.agenix.packages.${system}.default
+          inputs.nix-alien.packages.${system}.nix-alien
+        ];
+      }
+      ../hosts/nixos/configuration.nix
+    ];
+  };
+
   ### End WSL ###
 }
