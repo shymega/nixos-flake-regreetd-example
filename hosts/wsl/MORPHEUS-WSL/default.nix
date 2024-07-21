0832642cfe527c68ebbdf66ffe27e66ba13fa540
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-21 17:22:16 +0100

    refactor: Refactor and fix MORPHEUS-WSL host
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/wsl/MORPHEUS-WSL/default.nix
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{ config, ... }:
+{
+  networking.hostName = "MORPHEUS-WSL";
+  boot = {
+    supportedFilesystems = [ "ntfs" "zfs" ];
+    initrd.supportedFilesystems = [ "ntfs" "zfs" ];
+
+    extraModulePackages = with config.boot.kernelPackages; [ zfs ];
+  };
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  services = {
+    ollama = {
+      enable = false;
+      acceleration = "rocm";
+      sandbox = false;
+      models = "/data/AI/LLMs/Ollama/Models/";
+      writablePaths = [ "/data/AI/LLMs/Ollama/Models/" ];
+      environmentVariables = {
+        HSA_OVERRIDE_GFX_VERSION = "11.0.0"; # 780M
+      };
+    };
+  };
+
+  wsl = {
+    enable = true;
+    defaultUser = "dzrodriguez";
+  };
+}
