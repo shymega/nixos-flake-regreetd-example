9f66610ecace1ac2445f1d90010e2f18980dbf1d
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-21 23:28:38 +0100

    feat: Add dzrodriguez@TWINS-LINUX pubkeys
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- secrets/user/secrets.nix
+++ secrets/user/secrets.nix
@@ -4,9 +4,10 @@
 
 let
   dzrodriguez-NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7D0z5Unwjt00URZxRrx6T69PFc6xI3zHETtr0GbkM6";
+  dzrodriguez-TWINS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOZyVmZ3OdKl2f1kLSEnwwKaO8ecDKEbwLYXDAllIvU ";
   dominic.rodriguez-MORPHEUS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgAqM0gz24k8J1vqe3cp1MI48cSok6mtdMIYnT1d8CR";
   nixos-MORPHEUS-WSL = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbxdHlDVWPqpUTifSdvO3wMVg43u2coy+akmIYkaQ9W";
-  personal-users = [ dzrodriguez-NEO-LINUX dominic.rodriguez-MORPHEUS-LINUX nixos-MORPHEUS-WSL ];
+  personal-users = [ dzrodriguez-NEO-LINUX dzrodriguez-TWINS-LINUX dominic.rodriguez-MORPHEUS-LINUX nixos-MORPHEUS-WSL ];
 
   NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8Stawqd09idKurIZ+eSSEbmWdXIlQQJ4eaMo6bmClv";
   TWINS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMAp9kcBykTqbYroj9akZ7s6qY7NsX9uHwZMv64dOKvV";
