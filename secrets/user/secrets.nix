91f526ea8738cc2b92c42a145750d7005577287a
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-21 16:27:58 +0100

    chore: Update secrets
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- secrets/user/secrets.nix
+++ secrets/user/secrets.nix
@@ -5,16 +5,18 @@
 let
   dzrodriguez-NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7D0z5Unwjt00URZxRrx6T69PFc6xI3zHETtr0GbkM6";
   dominic.rodriguez-MORPHEUS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgAqM0gz24k8J1vqe3cp1MI48cSok6mtdMIYnT1d8CR";
-  personal-users = [ dzrodriguez-NEO-LINUX dominic.rodriguez-MORPHEUS-LINUX ];
+  nixos-MORPHEUS-WSL = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbxdHlDVWPqpUTifSdvO3wMVg43u2coy+akmIYkaQ9W";
+  personal-users = [ dzrodriguez-NEO-LINUX dominic.rodriguez-MORPHEUS-LINUX nixos-MORPHEUS-WSL ];
 
   NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8Stawqd09idKurIZ+eSSEbmWdXIlQQJ4eaMo6bmClv";
   TWINS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMAp9kcBykTqbYroj9akZ7s6qY7NsX9uHwZMv64dOKvV";
   MORPHEUS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqOAfNq3lGPElJ0L6qAqQLDykRWsN9dE4sMZkD6YVKu";
+  MORPHEUS-WSL = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEw1gq7SDBBKkEBN9k4YyekfMVC68TiPmZH38gCae0+T";
 
   DELTA-ZERO = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOBP4prVx3gdi5YMW4dzy06s46aobpyY8IlFBDVgjDU";
   DIAL-IN = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILd2G/XmmLSK4V+tBgkS62/qE4fsY8c0dYKyjkiYtqpX";
 
-  personal-machines = [ NEO-LINUX TWINS-LINUX MORPHEUS-LINUX ];
+  personal-machines = [ NEO-LINUX TWINS-LINUX MORPHEUS-LINUX MORPHEUS-WSL ]; 
   personal = personal-users ++ personal-machines;
 
   work-machines = [ ];
