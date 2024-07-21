0162f0b9e26bff486538937ae244580f5c2ea64a
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-21 23:28:38 +0100

    feat: Add dzrodriguez@TWINS-LINUX pubkeys
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- secrets/system/default.nix
+++ secrets/system/default.nix
@@ -6,6 +6,7 @@
   age = {
     identityPaths = [
       "/etc/ssh/ssh_host_ed25519_key"
+      "/persist/etc/ssh/ssh_host_ed25519_key"
     ];
     secrets = {
       postfix_sasl_passwd.file = ./postfix_sasl_passwd.age;
