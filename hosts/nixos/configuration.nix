8f4c1dfc300b17a35061fdbba8a974e7244a6af7
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-24 01:11:25 +0100

    feat: Add iDevice support
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -106,6 +106,10 @@ in
         DNS=1.1.1.1#1dot1dot1dot1.cloudflare-dns.com 1.0.0.1#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1111#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1001#1dot1dot1dot1.cloudflare-dns.com
       '';
     };
+    usbmuxd = {
+      enable = true;
+      package = pkgs.usbmuxd2;
+    };
   };
 
   networking = {
