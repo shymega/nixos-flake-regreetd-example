aeebebd879b4f80ab809f3a5b1d07e20d9fb4c84
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-22 20:03:40 +0100

    feat: Add more secrets
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- secrets/system/secrets.nix
+++ secrets/system/secrets.nix
@@ -1,42 +1,47 @@
-# SPDX-FileCopyrightText: 2023 Dom Rodriguez <shymega@shymega.org.uk>
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
 #
 # SPDX-License-Identifier: GPL-3.0-only
+
+#
 
 let
   dzrodriguez-NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7D0z5Unwjt00URZxRrx6T69PFc6xI3zHETtr0GbkM6";
   dzrodriguez-TWINS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOZyVmZ3OdKl2f1kLSEnwwKaO8ecDKEbwLYXDAllIvU ";
-  dominic.rodriguez-MORPHEUS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgAqM0gz24k8J1vqe3cp1MI48cSok6mtdMIYnT1d8CR";
-  personal-users = [ dzrodriguez-NEO-LINUX dzrodriguez-TWINS-LINUX dominic.rodriguez-MORPHEUS-LINUX ];
+  dzrodriguez-MORPHEUS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgAqM0gz24k8J1vqe3cp1MI48cSok6mtdMIYnT1d8CR";
+  personal-users = [
+    dzrodriguez-NEO-LINUX
+    dzrodriguez-TWINS-LINUX
+    dzrodriguez-MORPHEUS-LINUX
+  ];
 
   NEO-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8Stawqd09idKurIZ+eSSEbmWdXIlQQJ4eaMo6bmClv";
   TWINS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMAp9kcBykTqbYroj9akZ7s6qY7NsX9uHwZMv64dOKvV";
   MORPHEUS-LINUX = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqOAfNq3lGPElJ0L6qAqQLDykRWsN9dE4sMZkD6YVKu";
   MORPHEUS-WSL = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEw1gq7SDBBKkEBN9k4YyekfMVC68TiPmZH38gCae0+T";
 
-  DELTA-ZERO = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOBP4prVx3gdi5YMW4dzy06s46aobpyY8IlFBDVgjDU ";
+  DELTA-ZERO = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOBP4prVx3gdi5YMW4dzy06s46aobpyY8IlFBDVgjDU";
   DIAL-IN = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILd2G/XmmLSK4V+tBgkS62/qE4fsY8c0dYKyjkiYtqpX";
 
-  personal-machines = [ NEO-LINUX TWINS-LINUX MORPHEUS-LINUX MORPHEUS-WSL ];
+  personal-machines = [
+    NEO-LINUX
+    TWINS-LINUX
+    MORPHEUS-LINUX
+    MORPHEUS-WSL
+  ];
   personal = personal-users ++ personal-machines;
 
-  work-machines = [ ];
-  work-users = [ ];
-  work = work-machines ++ work-users;
-
-  rnet-machines = [ DELTA-ZERO DIAL-IN ];
+  rnet-machines = [
+    DELTA-ZERO
+    DIAL-IN
+  ];
   rnet-users = [ ];
   rnet = rnet-machines ++ rnet-users;
-
-  all-machines = personal-machines ++ work-machines ++ rnet-machines;
-  all-users = personal-users ++ work-users ++ rnet-users;
-
-  allKeys = all-machines ++ all-users;
 in
 {
   "postfix_sasl_passwd.age".publicKeys = personal ++ rnet;
   "postfix_sender_relay.age".publicKeys = personal ++ rnet;
-  "user_dominic.rodriguez.age".publicKeys = personal ++ rnet;
+  "dzrodriguez.age".publicKeys = personal ++ rnet;
   "geoclue_url.age".publicKeys = personal-machines ++ rnet;
   "zerotier_networks.age".publicKeys = personal-machines ++ rnet;
-  "home_network_iot_p.age".publicKeys = personal-machines ++ rnet-machines;
+  "wireless.age".publicKeys = personal-machines ++ rnet-machines;
 }
