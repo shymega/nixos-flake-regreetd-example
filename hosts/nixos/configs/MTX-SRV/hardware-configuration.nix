abdaee7b1b0bae6219c2ae64de7f7eb8f25ca93f
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-22 13:45:36 +0100

    fix: Fix MTX-SRV cfg
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configs/MTX-SRV/hardware-configuration.nix
+++ hosts/nixos/configs/MTX-SRV/hardware-configuration.nix
@@ -2,14 +2,21 @@
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot = {
-    loader.systemd-boot.enable = lib.mkForce false;
-    loader.grub = {
-      enable = true;
-      efiSupport = lib.mkForce false;
-      devices = [ "/dev/sdas" ];
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+      systemd-boot.enable = lib.mkForce false;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = lib.mkForce "nodev";
+      };
     };
     initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
     initrd.kernelModules = [ "nvme" ];
   };
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  fileSystems."/boot/efi" = { device = "/dev/sda2"; fsType = "vfat"; };
 }
