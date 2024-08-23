ee09a0c9f99f25f570f6cd2182a4a0473a6a5b0e
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-23 16:28:30 +0100

    feat: Add support for WhatsApp
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configs/MTX-SRV/synapse.nix
+++ hosts/nixos/configs/MTX-SRV/synapse.nix
@@ -70,6 +70,52 @@ in
         ./extra_synapse_conf.yaml
       ];
     };
+
+    mautrix-whatsapp = {
+      settings = {
+        appservice = {
+          as_token = "";
+          bot = {
+            displayname = "WhatsApp Bridge Bot";
+            username = "whatsappbot";
+          };
+          database = {
+            type = "sqlite3";
+            uri = "/var/lib/mautrix-whatsapp/mautrix-whatsapp.db";
+          };
+          hostname = "[::]";
+          hs_token = "";
+          id = "whatsapp";
+          port = 29318;
+        };
+        bridge = {
+          command_prefix = "!wa";
+          displayname_template = "{{if .BusinessName}}{{.BusinessName}}{{else if .PushName}}{{.PushName}}{{else}}{{.JID}}{{end}} (WA)";
+          double_puppet_server_map = { };
+          login_shared_secret_map = { };
+          permissions = {
+            "*" = "relay";
+          };
+          relay = {
+            enabled = true;
+          };
+          username_template = "whatsapp_{{.}}";
+        };
+        homeserver = {
+          address = "http://localhost:8008";
+        };
+        logging = {
+          min_level = "info";
+          writers = [
+            {
+              format = "pretty-colored";
+              time_format = " ";
+              type = "stdout";
+            }
+          ];
+        };
+      };
+    };
   };
 }
 
