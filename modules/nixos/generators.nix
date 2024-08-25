4e92eefe16c8439f25f2c3a59ef2c4dcd0edb3aa
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-25 22:41:48 +0100

    feat: Add nixos-generators support
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- modules/nixos/generators.nix
+++ modules/nixos/generators.nix
@@ -1,7 +1,23 @@
-{ inputs, system, ... }: {
+{ inputs, system, ... }:
+let
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+  };
+  inherit (pkgs) lib;
+in
+{
   imports = [
     inputs.nixos-generators.nixosModules.all-formats
   ];
+
+  formatConfigs.proxmox-lxc = {
+    proxmoxLXC.manageHostName = true;
+  };
+
+  formatConfigs.docker = {
+    networking.firewall.enable = lib.mkForce false;
+    services.fail2ban.enable = lib.mkForce false;
+  };
 
   nixpkgs.hostPlatform = system;
 }
