02b5a48de80bae05ce6e030d36bd9efa7ff5e7c2
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-25 14:05:51 +0100

    fix: Fix Synapse
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/configs/MTX-SRV/postgres.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  services = {
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "mautrix_slack" "mautrix_whatsapp" "mautrix_meta_facebook" "mautrix_meta_instagram" "mautrix_meta_messenger" "mautrix_telegram" ];
+      enableTCPIP = true;
+      settings.port = 5432;
+      authentication = pkgs.lib.mkOverride 10 ''
+        #...
+        #type database DBuser origin-address auth-method
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        # ipv6
+        host all       all     ::1/128        trust
+        #type database  DBuser  auth-method
+        local all       all     trust
+      '';
+      initialScript = pkgs.writeText "backend-initScript" ''
+        CREATE ROLE matrix WITH LOGIN PASSWORD 'matrix4me' CREATEDB;
+        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to matrix;
+      '';
+    };
+  };
+}
---
+++ overlays/stable/mautrix-slack.nix
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+(_finaL: prev: { mautrix-slack = prev.callPackage ../../pkgs/mautrix-slack { }; })
