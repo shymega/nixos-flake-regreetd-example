9b8b7b7f43dd763cf3a313cc49f91f186f780336
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-25 18:46:45 +0100

    fix: Fix DNS on MTX-SRV
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configs/MTX-SRV/default.nix
+++ hosts/nixos/configs/MTX-SRV/default.nix
@@ -61,10 +61,10 @@
     };
     resolved = {
       enable = true;
-      dnssec = "true";
-      dnsovertls = "true";
+      dnssec = "false";
+      dnsovertls = "false";
       extraConfig = ''
-        DNS=1.1.1.1#1dot1dot1dot1.cloudflare-dns.com 1.0.0.1#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1111#1dot1dot1dot1.cloudflare-dns.com 2606:4700:4700::1001#1dot1dot1dot1.cloudflare-dns.com
+        DNS=1.1.1.1 1.0.0.1
       '';
     };
   };
