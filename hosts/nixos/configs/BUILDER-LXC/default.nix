1b2c0ac8c65992edfd42c790e1f07db0d6e1f55a
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-26 19:16:56 +0100

    feat: Add initial LXC iamge for builds
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/configs/BUILDER-LXC/default.nix
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{
+  networking.hostName = "BUILDER-LXC";
+
+  system.stateVersion = "24.05";
+
+}
