87d7b00ab9d36d20e2035bdd39822ed8acb5ab35
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-27 00:36:27 +0100

    revert: Revert "fix: Update secrets"
    This reverts commit b1f293dc5bd16aa5401d201b2b885b9740ded2dc.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- secrets/system/default.nix
+++ secrets/system/default.nix
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
+#
+
 {
   age = {
     identityPaths = [
@@ -12,51 +14,49 @@
       postfix_sasl_passwd = {
         file = ./postfix_sasl_passwd.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       postfix_sender_relay = {
         file = ./postfix_sender_relay.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       dzrodriguez = {
         file = ./dzrodriguez.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       geoclue_url = {
         file = ./geoclue_url.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       zerotier_networks = {
         file = ./zerotier_networks.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       wireless = {
         file = ./wireless.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       synapse_secret = {
         file = ./synapse_secret.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       cloudflare_dns_token = {
         file = ./cloudflare_dns_token.age;
         group = "users";
-        mode = "0640";
+        mode = "640";
       };
       nixbuild_ssh_priv_key = {
         file = ./nixbuild_ssh_priv_key.age;
-        group = "users";
         mode = "0600";
       };
       nixbuild_ssh_pub_key = {
         file = ./nixbuild_ssh_pub_key.age;
-        group = "users";
         mode = "0600";
       };
     };
