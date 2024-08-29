296c951ad2e0114c1723a8f1557b9c064d5b84c1
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-29 14:13:02 +0100

    refactor: Use Synapse workers to help with sync
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configs/DELTA-ZERO/postgres.nix
+++ hosts/nixos/configs/DELTA-ZERO/postgres.nix
@@ -3,7 +3,6 @@
   services = {
     postgresql = {
       enable = true;
-      ensureDatabases = [ "mautrix_slack" "mautrix_whatsapp" "mautrix_meta_facebook" "mautrix_meta_instagram" "mautrix_meta_messenger" "mautrix_telegram" "matrix_synapse_syncv3" ];
       enableTCPIP = true;
       settings.port = 5432;
       authentication = pkgs.lib.mkOverride 10 ''
@@ -18,8 +17,64 @@
       '';
       initialScript = pkgs.writeText "backend-initScript" ''
         CREATE ROLE matrix WITH LOGIN PASSWORD 'matrix4me' CREATEDB;
-        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to matrix;
       '';
+      ensureDatabases = [ "mautrix_slack" "mautrix_whatsapp" "mautrix_meta_facebook" "mautrix_meta_instagram" "mautrix_meta_messenger" "mautrix_telegram" "matrix_synapse_syncv3" ];
+
+      settings = {
+        # https://pgconfigurator.cybertec.at/
+        max_connections = 1850;
+        superuser_reserved_connections = 3;
+
+        shared_buffers = "2GB";
+        work_mem = "16GB";
+        maintenance_work_mem = "8GB";
+        huge_pages = "try";
+        effective_cache_size = "64GB"; # was 22
+        effective_io_concurrency = 100;
+        random_page_cost = 1.1;
+
+        # can use this to view stats: SELECT query, total_time, calls, rows FROM pg_stat_statements ORDER BY total_time DESC LIMIT 10;
+        shared_preload_libraries = "pg_stat_statements";
+        track_io_timing = "on";
+        track_functions = "pl";
+        "pg_stat_statements.max" = "10000"; # additional
+        "pg_stat_statements.track" = "all"; # additional
+
+        wal_level = "replica";
+        max_wal_senders = 0;
+        synchronous_commit = "off"; # was ond3
+
+        checkpoint_timeout = "15min";
+        checkpoint_completion_target = "0.9";
+        max_wal_size = "2GB";
+        min_wal_size = "1GB";
+
+        wal_compression = "on";
+        wal_buffers = "-1";
+        wal_writer_delay = "200ms";
+        wal_writer_flush_after = "1MB";
+        #checkpoint_segments = "64"; # additional
+        default_statistics_target = "250"; # additional
+
+        bgwriter_delay = "200ms";
+        bgwriter_lru_maxpages = "100";
+        bgwriter_lru_multiplier = "2.0";
+        bgwriter_flush_after = "0";
+
+        max_worker_processes = "32"; # was 14
+        max_parallel_workers_per_gather = "16"; # was 7
+        max_parallel_maintenance_workers = "16"; # was 7
+        max_parallel_workers = "32"; # was 14
+        parallel_leader_participation = "on";
+
+        enable_partitionwise_join = "on";
+        enable_partitionwise_aggregate = "on";
+        jit = "on";
+        max_slot_wal_keep_size = "1GB";
+        track_wal_io_timing = "on";
+        maintenance_io_concurrency = "4";
+        wal_recycle = "on";
+      };
     };
   };
 }
---
+++ hosts/nixos/configs/DELTA-ZERO/workers/sync.nix
@@ -0,0 +1,125 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.services.matrix-synapse;
+  dbGroup = "small";
+  workers = lib.range 0 (cfg.syncWorkers - 1);
+  workerName = "sync";
+  workerRoutes = {
+    client = [
+      "~ ^/_matrix/client/(v2_alpha|r0|v3)/sync$"
+      "~ ^/_matrix/client/(api/v1|v2_alpha|r0|v3)/events$"
+      "~ ^/_matrix/client/(api/v1|r0|v3)/initialSync$"
+      "~ ^/_matrix/client/(api/v1|r0|v3)/rooms/[^/]+/initialSync$"
+    ];
+    federation = [ ];
+    media = [ ];
+  };
+
+  enabledResources = lib.optionals (lib.length workerRoutes.client > 0) [ "client" ] ++ lib.optionals (lib.length workerRoutes.federation > 0) [ "federation" ] ++ lib.optionals (lib.length workerRoutes.media > 0) [ "media" ];
+in
+{
+  config = lib.mkIf (cfg.syncWorkers > 0) {
+    services.matrix-synapse = {
+      settings = {
+        instance_map = lib.listToAttrs (
+          lib.map
+            (index: {
+              name = "${workerName}-${toString index}";
+              value = {
+                path = "/run/matrix-synapse/${workerName}-${toString index}.sock";
+              };
+            })
+            workers
+        );
+      };
+
+      workers = lib.listToAttrs (
+        lib.map
+          (index: {
+            name = "${workerName}-${toString index}";
+            value = {
+              worker_app = "synapse.app.generic_worker";
+              worker_listeners =
+                [
+                  {
+                    type = "http";
+                    path = "/run/matrix-synapse/${workerName}-${toString index}.sock";
+                    resources = [
+                      {
+                        names = [ "replication" ];
+                        compress = false;
+                      }
+                    ];
+                  }
+                ]
+                ++ lib.map
+                  (type: {
+                    type = "http";
+                    path = "/run/matrix-synapse/${workerName}-${type}-${toString index}.sock";
+                    mode = "666";
+                    resources = [
+                      {
+                        names = [ type ];
+                        compress = false;
+                      }
+                    ];
+                  })
+                  enabledResources;
+              database = import ../db.nix {
+                inherit dbGroup;
+                workerName = "${workerName}-${toString index}";
+              };
+            };
+          })
+          workers
+      );
+    };
+
+    services.nginx = {
+      upstreams = lib.listToAttrs (
+        lib.map
+          (type: {
+            name = "${workerName}-${type}";
+            value = {
+              extraConfig = ''
+                keepalive 32;
+                least_conn;
+              '';
+              servers = lib.listToAttrs (
+                lib.map
+                  (index: {
+                    name = "unix:/run/matrix-synapse/${workerName}-${type}-${toString index}.sock";
+                    value = {
+                      max_fails = 0;
+                    };
+                  })
+                  workers
+              );
+            };
+          })
+          enabledResources
+      );
+
+      virtualHosts."${cfg.nginxVirtualHostName}".locations = lib.listToAttrs (
+        lib.flatten (
+          lib.forEach enabledResources (
+            type:
+            lib.map
+              (route: {
+                name = route;
+                value = {
+                  proxyPass = "http://${workerName}-${type}";
+                  extraConfig = ''
+                    proxy_http_version 1.1;
+                    proxy_set_header Connection "";
+                  '';
+                };
+              })
+              workerRoutes.${type}
+          )
+        )
+      );
+    };
+  };
+}
