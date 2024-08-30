1ecc61ccb5b96c2a03fb1f3c80e04df5c7aaa5d0
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-30 15:58:31 +0100

    feat: Add initial revsison of my Doom Emacs config
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- homes/configs/doom.d/packages.el
+++ homes/configs/doom.d/packages.el
@@ -7,17 +7,14 @@
 ;;; $DOOMDIR/packages.el
 
 (package! evil-escape :disable t)
-
+(package! w3m)
+(package! erc-hl-nicks)
 (package! dotenv-mode)
-
 (package! 1password
   :recipe (:host github :repo "kamushadenes/1password.el" :files ("*.el"))
   :pin "d493405ef663654a497f9586788dfff7de26e9de")
-
 (package! darkman
    :recipe (:host nil :repo "https://git.sr.ht/~grtcdr/darkman.el" :files ("*.el"))
    :pin "136eac628595c6777eb6b2246a014dfcb3b6c625")
-
 (package! direnv)
-
 (package! ement)
