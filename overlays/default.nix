fba3fcee4c137a2863fc29291d1781985a812acb
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-31 14:49:14 +0100

    fix: Remove unused overlay/module
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/default.nix
+++ overlays/default.nix
@@ -32,8 +32,8 @@ let
   ];
 
   customOverlays = [
-    (import ./master.nix { inherit inputs lib; })
-    (import ./shymega.nix { inherit inputs lib; })
+    #    (import ./master.nix { inherit inputs lib; })
+    #    (import ./shymega.nix { inherit inputs lib; })
     (import ./unstable.nix { inherit inputs lib; })
   ];
 in
