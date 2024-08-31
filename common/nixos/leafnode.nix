86b36214b6bcb2d6a00b4a09fe1913bbff7a874f
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-31 22:18:47 +0100

    chore: Run `nix fmt` on codebase
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/leafnode.nix
+++ common/nixos/leafnode.nix
@@ -1,42 +1,44 @@
 { pkgs
+, config
 , ...
 }:
 {
   systemd = {
     services."leafnode@" = {
+      requires = [ "leafnode.socket" ];
       enable = true;
-      unitConfig =  {
-  RefuseManualStart = true;
-  RefuseManualStop = true;
-      Description = "Leafnode listening on %i"; };
+      unitConfig = {
+        RefuseManualStart = true;
+        RefuseManualStop = true;
+        Description = "Leafnode listening on %i";
+      };
       serviceConfig = {
-      DynamicUser = true;
-            CapabilityBoundingSet = [ "" ];
-            LockPersonality = true;
-            NoNewPrivileges = true;
-            PrivateDevices = true;
-            PrivateTmp = true;
-            PrivateUsers = true;
-            ProcSubset = "pid";
-            ProtectClock = true;
-            ProtectControlGroups = true;
-            ProtectHome = true;
-            ProtectHostname = true;
-            ProtectKernelLogs = true;
-            ProtectKernelModules = true;
-            ProtectKernelTunables = true;
-            ProtectProc = "invisible";
-            ProtectSystem = "strict";
-            ReadOnlyPaths = [ "/etc/leafnode/config" ];
-            ReadWritePaths = [ "/var/spool/leafnode" ];
-            RemoveIPC = true;
-            RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
-            RestrictNamespaces = true;
-            RestrictRealtime = true;
-            RestrictSUIDSGID = true;
-            SystemCallArchitectures = "native";
-            SystemCallFilter = [ "@system-service" "~@resources" "~@privileged" ];
-        Type = "simple";
+        DynamicUser = true;
+        CapabilityBoundingSet = [ "" ];
+        LockPersonality = true;
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProcSubset = "pid";
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        ReadOnlyPaths = [ "/etc/leafnode/config" ];
+        ReadWritePaths = [ "/var/spool/leafnode" ];
+        RemoveIPC = true;
+        RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [ "@system-service" "~@resources" "~@privileged" ];
         ExecStart = "${pkgs.leafnode}/bin/leafnode -d /var/spool/leafnode -F /etc/leafnode/config";
         StandardInput = "socket";
         StandardOutput = "socket";
@@ -47,7 +49,8 @@
       enable = true;
       wantedBy = [ "sockets.target" ];
       description = "Leafnode listener";
-      socketConfig.ListenStream = "127.0.0.1:119";
+      listenStreams = [ "127.0.0.1:119" ];
+      socketConfig.Accept = true;
     };
   };
 }
