ed06b3d0412d3b1b2eec89b662228c83b28b6752
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-02 00:41:47 +0100

    fix: Tidy up generators
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- nix/generators.nix
+++ nix/generators.nix
@@ -3,7 +3,7 @@ let
   configs = self.nixosConfigurations;
 in
 rec {
-  MTX-SRV = configs.MTX-SRV.config.formats.proxmox-lxc;
+  MATRIX-SERVER = configs.MATRIX-SERVER.config.formats.proxmox-lxc;
 
   INSTALLER-SERVER-ISO-ARM = configs.INSTALLER-SERVER-ISO-ARM.config.formats.install-iso;
   INSTALLER-SERVER-ISO-X86 = configs.INSTALLER-SERVER-ISO-X86.config.formats.install-iso;
@@ -11,5 +11,5 @@ rec {
   INSTALLER-WORKSTATION-ISO-ARM = configs.INSTALLER-WORKSTATION-ISO-ARM.config.formats.install-iso;
   INSTALLER-WORKSTATION-ISO-X86 = configs.INSTALLER-WORKSTATION-ISO-X86.config.formats.install-iso;
 
-  all = MTX-SRV INSTALLER-SERVER-ISO-ARM INSTALLER-SERVER-ISO-X86 INSTALLER-WORKSTATION-ISO-ARM INSTALLER-WORKSTATION-ISO-X86;
+  all = MATRIX-SERVER // INSTALLER-SERVER-ISO-ARM // INSTALLER-SERVER-ISO-X86 // INSTALLER-WORKSTATION-ISO-ARM // INSTALLER-WORKSTATION-ISO-X86;
 }
