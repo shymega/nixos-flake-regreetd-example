6418de0ec1cbf94e41fad94816ef41d0200d478a
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-01 10:39:43 +0100

    refactor: Refactor `udev` automount rules
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/automount.nix
+++ common/nixos/automount.nix
@@ -1,16 +1,32 @@
-_:
+{ pkgs
+, ...
+}:
 {
-  fileSystems = {
-    "/mnt/pi/pico" = {
-      device = "/dev/disk/by-label/RPI-RP2";
-      fsType = "vfat";
-      options = [
-        "nofail"
-        "uid=1001"
-        "gid=100"
-        "nofail"
-        "x-systemd.automount"
-      ];
-    };
-  };
+  system.activationScripts.rp2040-mountpoint = ''
+    ${pkgs.coreutils}/bin/mkdir -pv /mnt/dev/rp2040
+  '';
+
+  services.udev.extraRules = ''
+    # RP2040 Pico
+     ACTION=="add|change" \
+    , SUBSYSTEMS=="usb" \
+    , SUBSYSTEM=="block" \
+    , ENV{ID_FS_USAGE}=="filesystem" \
+    , ENV{ID_FS_LABEL}=="RPI-RP2" \
+    , RUN{program}+="${pkgs.systemd}/bin/systemd-mount --owner=1001 --no-block --collect $devnode /mnt/dev/rp2040"
+
+
+    # USB storage devices:
+      ACTION=="add|change" \
+    , SUBSYSTEM=="block" \
+    , ENV{ID_FS_USAGE}=="filesystem" \
+    , ENV{ID_FS_LABEL}!="RPI-RP2" \
+    , RUN{program}+="${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --owner=1001 --collect $devnode /run/media/system/$env{ID_FS_UUID}"
+
+    ACTION=="remove" \
+    , SUBSYSTEM=="block" \
+    , ENV{ID_FS_USAGE}=="filesystem" \
+    , ENV{ID_FS_LABEL}!="RPI-RP2" \
+    , RUN{program}+="${pkgs.systemd}/bin/systemd-mount --umount /run/media/system/$env{ID_FS_UUID}"
+  '';
 }
