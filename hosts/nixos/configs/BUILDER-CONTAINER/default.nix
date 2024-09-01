13b63766ddf46e4b5d1b46cb2912ad262a7f5851
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-02 00:32:45 +0100

    chore: Tidy up BUILD*
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configs/BUILDER-CONTAINER/default.nix
+++ hosts/nixos/configs/BUILDER-CONTAINER/default.nix
@@ -2,7 +2,17 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
+{ modulesPath, lib, ...}:
 {
+  imports = [
+    "${toString modulesPath}/virtualisation/docker-image.nix"
+  ];
+
+  boot.isContainer = true;
+  boot.loader.grub.enable = lib.mkForce false;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  services.journald.console = "/dev/console";
+
   networking.hostName = "BUILDER-CONTAINER";
 
   system.stateVersion = "24.05";
