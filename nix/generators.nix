c5cf21ffb9d9fc3293a39d9d6145bef3367f2e2f
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-02 20:17:19 +0100

    fix: Fix wrong hostname nix/generators.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- nix/generators.nix
+++ nix/generators.nix
@@ -3,7 +3,7 @@ let
   configs = self.nixosConfigurations;
 in
 rec {
-  MATRIX-SERVER = configs.MATRIX-SERVER.config.formats.proxmox-lxc;
+  MATRIX-LXC = configs.MATRIX-LXC.config.formats.proxmox-lxc;
 
   INSTALLER-SERVER-ISO-ARM = configs.INSTALLER-SERVER-ISO-ARM.config.formats.install-iso;
   INSTALLER-SERVER-ISO-X86 = configs.INSTALLER-SERVER-ISO-X86.config.formats.install-iso;
@@ -11,5 +11,5 @@ rec {
   INSTALLER-WORKSTATION-ISO-ARM = configs.INSTALLER-WORKSTATION-ISO-ARM.config.formats.install-iso;
   INSTALLER-WORKSTATION-ISO-X86 = configs.INSTALLER-WORKSTATION-ISO-X86.config.formats.install-iso;
 
-  all = MATRIX-SERVER // INSTALLER-SERVER-ISO-ARM // INSTALLER-SERVER-ISO-X86 // INSTALLER-WORKSTATION-ISO-ARM // INSTALLER-WORKSTATION-ISO-X86;
+  all = MATRIX-LXC // INSTALLER-SERVER-ISO-ARM // INSTALLER-SERVER-ISO-X86 // INSTALLER-WORKSTATION-ISO-ARM // INSTALLER-WORKSTATION-ISO-X86;
 }
