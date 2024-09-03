9ce54de156731c0c451adab56cbcaad09c7cb0c3
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-03 16:42:45 +0100

    fix: Fix UID mismatch in `automount`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/automount.nix
+++ common/nixos/automount.nix
@@ -13,7 +13,7 @@
     , SUBSYSTEM=="block" \
     , ENV{ID_FS_USAGE}=="filesystem" \
     , ENV{ID_FS_LABEL}=="RPI-RP2" \
-    , RUN{program}+="${pkgs.systemd}/bin/systemd-mount --owner=1001 --no-block --collect $devnode /mnt/dev/rp2040"
+    , RUN{program}+="${pkgs.systemd}/bin/systemd-mount --owner=1000 --no-block --collect $devnode /mnt/dev/rp2040"
 
 
     # USB storage devices:
@@ -21,7 +21,7 @@
     , SUBSYSTEM=="block" \
     , ENV{ID_FS_USAGE}=="filesystem" \
     , ENV{ID_FS_LABEL}!="RPI-RP2" \
-    , RUN{program}+="${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --owner=1001 --collect $devnode /run/media/system/$env{ID_FS_UUID}"
+    , RUN{program}+="${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --owner=1000 --collect $devnode /run/media/system/$env{ID_FS_UUID}"
 
     ACTION=="remove" \
     , SUBSYSTEM=="block" \
