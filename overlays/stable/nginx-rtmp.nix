c727d5f43f0164b006db06e96f490c9a36bdcb01
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-04 12:54:20 +0100

    feat: Add nginx-rtmp - for livestreaming
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ overlays/stable/nginx-rtmp.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+_final: prev: {
+  nginx-rtmp-patched = prev.nginxStable.override(oldAttrs: {
+    pname = "nginx-rtmp-patched";
+    version = "stable";
+    modules = oldAttrs.modules ++ [ prev.nginxModules.rtmp ];
+  });
+}
