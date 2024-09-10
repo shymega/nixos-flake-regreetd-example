b77b6a3b211f3605b77c7ab967a622d41b7730a4
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-10 21:59:43 +0100

    fix: Remove overidding attr
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configs/MORPHEUS-JOVIAN/hardware-configuration.nix
+++ hosts/nixos/configs/MORPHEUS-JOVIAN/hardware-configuration.nix
@@ -34,8 +34,6 @@
     };
   };
 
-  jovian.devices.steamdeck.enable = true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
