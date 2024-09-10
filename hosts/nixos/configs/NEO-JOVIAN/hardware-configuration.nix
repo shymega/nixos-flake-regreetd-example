f86fbc2fbbffd312272c41d5210d26799d60e9f3
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-10 11:45:41 +0100

    feat: Add NEO-JOVIAN
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/configs/NEO-JOVIAN/hardware-configuration.nix
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{ pkgs, inputs, config, lib, ... }:
+{
+  imports = [
+    ./disks.nix
+    inputs.jovian-nixos.nixosModules.default
+  ];
+
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        memtest86.enable = true;
+        netbootxyz.enable = true;
+        extraFiles = {
+          "efi/shell/shellx64.efi" = "${pkgs.edk2-uefi-shell}/shell.efi";
+        };
+        extraEntries = {
+          "shell.conf" = ''
+            title UEFI shell
+            efi /EFI/SHELL/SHELLX64.EFI
+          '';
+        };
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+      generationsDir.copyKernels = true;
+      timeout = 6;
+    };
+  };
+
+  jovian.devices.steamdeck.enable = true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
