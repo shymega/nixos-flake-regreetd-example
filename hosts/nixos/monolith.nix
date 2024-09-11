79fc5108686f417709a8d77fc079028f73ec4960
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-11 11:46:46 +0000

    chore: Downgrade 1Password
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/monolith.nix
+++ hosts/nixos/monolith.nix
@@ -151,11 +151,11 @@
 
     _1password = {
       enable = true;
-      package = pkgs.unstable._1password;
+      package = pkgs._1password;
     };
     _1password-gui = {
       enable = true;
-      package = pkgs.unstable._1password-gui;
+      package = pkgs._1password-gui;
       polkitPolicyOwners = [ "dzrodriguez" ];
     };
   };
