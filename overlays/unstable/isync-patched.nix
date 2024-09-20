e8e1607d40c17c315dee58a1082cb02ec7f5a953
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-20 18:15:51 +0100

    revert: Revert overlay change to `isync-patched`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/unstable/isync-patched.nix
+++ overlays/unstable/isync-patched.nix
@@ -1,27 +1,17 @@
-# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+# SPDX-FileCopyrightText: 2023 Dom Rodriguez <shymega@shymega.org.uk>
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-#
-
-_final: prev: {
-  isync-patched = prev.isync.overrideAttrs (_oldAttrs: rec {
+final: prev: {
+  isync-patched = prev.isync.overrideAttrs (oldAttrs: rec {
     pname = "isync";
     version = "1.5.0";
-    src = prev.fetchgit {
-      url = "https://git.code.sf.net/p/isync/isync";
-      rev = "0af93316ff2d62472e3faa23420180910b32d858";
-      sha256 = "sha256-yqe+qiZLletTuVD9pQKDX57wCBh2LoOr2QBJFBNnhNE=";
+    src = prev.fetchurl {
+      url = "mirror://sourceforge/isync/${pname}-${version}.tar.gz";
+      sha256 = "sha256-oMgeEJOHvyedoWFFMQM5nneUav7PXFH5QTxedzVX940=";
     };
     withCyrusSaslXoauth2 = true;
     dontPatch = true;
-
-    preConfigure = ''
-      touch ChangeLog
-      ./autogen.sh
-    '';
-
-    nativeBuildInputs = with prev; [ autoconf automake perl pkg-config ];
-    buildInputs = with prev; [ cyrus_sasl db openssl zlib wrapProgram ];
   });
 }
+
