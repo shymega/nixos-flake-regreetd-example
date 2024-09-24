a86a295523ecd1490d4ed2b64b49730046d2ba4f
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-24 23:56:29 +0200

    fix: Fix missing brace
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- hosts/nixos/configs/NEO-LINUX/default.nix
+++ hosts/nixos/configs/NEO-LINUX/default.nix
@@ -55,183 +55,183 @@ in
           unitConfig.DefaultDependencies = "no";
         };
       };
-
-      kernelPackages =
-        if enableXanmod then
-          pkgs.linuxPackages_xanmod
-        else
-          config.boot.zfs.package.latestCompatibleLinuxPackages;
-
-      extraModulePackages = with config.boot.kernelPackages; [ zfs ];
-
-      extraModprobeConfig = ''
-        options kvm_amd nested=1
-        options kvm ignore_msrs=1 report_ignored_msrs=0
-      '';
-
-      zfs.devNodes = "/dev/NEO-LINUX/ROOT";
-
-      kernel.sysctl = {
-        "fs.inotify.max_user_watches" = "819200";
-        "kernel.printk" = "3 3 3 3";
-      };
-
-      plymouth = {
-        enable = true;
-      };
-
-      loader = {
-        systemd-boot = {
-          enable = false;
-        };
-        efi = {
-          canTouchEfiVariables = true;
-          efiSysMountPoint = "/boot";
-        };
-        grub = {
-          device = "nodev";
-          efiSupport = true;
-          default = "saved";
-          enable = true;
-          useOSProber = true;
-        };
-        timeout = 6;
-      };
     };
 
-    hardware = {
-      graphics = {
-        enable = true;
-        enable32Bit = true;
-        extraPackages = with pkgs; [
-          amdvlk
-          # VA-API and VDPAU
-          vaapiVdpau
+    kernelPackages =
+      if enableXanmod then
+        pkgs.linuxPackages_xanmod
+      else
+        config.boot.zfs.package.latestCompatibleLinuxPackages;
 
-          # AMD ROCm OpenCL runtime
-          rocmPackages.clr
-          rocmPackages.clr.icd
-        ];
-        extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
-      };
-      amdgpu = {
-        amdvlk = {
-          enable = true;
-          support32Bit.enable = true;
-        };
-        opencl.enable = true;
-      };
-      cpu.amd.ryzen-smu.enable = true;
+    extraModulePackages = with config.boot.kernelPackages; [ zfs ];
+
+    extraModprobeConfig = ''
+      options kvm_amd nested=1
+      options kvm ignore_msrs=1 report_ignored_msrs=0
+    '';
+
+    zfs.devNodes = "/dev/NEO-LINUX/ROOT";
+
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "819200";
+      "kernel.printk" = "3 3 3 3";
     };
 
-    services.ollama = {
+    plymouth = {
       enable = true;
-      package = pkgs.unstable.ollama;
-      acceleration = "rocm";
-      sandbox = false;
-      models = "/data/AI/LLMs/Ollama/Models/";
-      environmentVariables = {
-        HSA_OVERRIDE_GFX_VERSION = "10.3.0"; # 680M.
-      };
     };
 
-    boot.binfmt.emulatedSystems = [
-      "aarch64-linux"
-      "armv6l-linux"
-      "armv7l-linux"
-      "i686-linux"
-    ];
-
-    services = {
-      udev = {
-        packages = with pkgs; [ gnome.gnome-settings-daemon ];
-        extraRules = ''
-          SUBSYSTEM=="power_supply", KERNEL=="ADP1", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl --no-block start ac.target"
-
-          # Workstation - keyboard & mouse suspension.
-          ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="024f", ATTR{power/autosuspend}="-1"
-          ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="1bcf", ATTR{idProduct}=="0005", ATTR{power/autosuspend}="-1"
-
-          # KVM switch target.
-          SUBSYSTEM=="usb", ACTION=="add|change|remove", ATTR{idVendor}=="1bcf", ATTR{idProduct}=="0005",  SYMLINK+="kvm-active", TAG+="systemd"
-
-          # Rename network interface.
-          SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{DEVTYPE}=="wlan", KERNEL=="wlan*", NAME="wlan0"
-
-          # My personal iPhone.
-          SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="12a8", KERNEL=="eth*", NAME="iphone0"
-
-          # My personal OP6T.
-          SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="9024", KERNEL=="usb*", NAME="android0"
-
-        '';
+    loader = {
+      systemd-boot = {
+        enable = false;
       };
-
-      zfs = {
-        trim = {
-          enable = true;
-          interval = "Sat *-*-* 04:00:00";
-        };
-        autoScrub = {
-          enable = true;
-          interval = "Sat *-*-* 05:00:00";
-        };
-        autoSnapshot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
       };
-      xserver = {
+      grub = {
+        device = "nodev";
+        efiSupport = true;
+        default = "saved";
         enable = true;
-        videoDrivers = [ "amdgpu" ];
+        useOSProber = true;
       };
-      auto-cpufreq.enable = false;
-      power-profiles-daemon.enable = pkgs.lib.mkForce false;
-      thermald.enable = true;
+      timeout = 6;
     };
+  };
 
-    powerManagement = {
+  hardware = {
+    graphics = {
       enable = true;
-      cpuFreqGovernor = "performance";
-    };
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        amdvlk
+        # VA-API and VDPAU
+        vaapiVdpau
 
-    programs.steam = {
-      enable = true;
-      gamescopeSession.enable = false;
-      package = pkgs.steam.override {
-        extraPkgs =
-          pkgs: with pkgs; [
-            #          deckcheatz
-            protontricks
-            protonup-qt
-            python3Full
-            python3Packages.pip
-            python3Packages.virtualenv
-            steamcmd
-            steamtinkerlaunch
-            wemod-launcher
-            wineWowPackages.stable
-            winetricks
-          ];
-      };
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-    };
-
-    security = {
-      pam.loginLimits = [
-        {
-          domain = "*";
-          item = "nofile";
-          type = "-";
-          value = "524288";
-        }
-        {
-          domain = "*";
-          item = "memlock";
-          type = "-";
-          value = "524288";
-        }
+        # AMD ROCm OpenCL runtime
+        rocmPackages.clr
+        rocmPackages.clr.icd
       ];
+      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
     };
-    system.stateVersion = "24.05";
+    amdgpu = {
+      amdvlk = {
+        enable = true;
+        support32Bit.enable = true;
+      };
+      opencl.enable = true;
+    };
+    cpu.amd.ryzen-smu.enable = true;
+  };
 
-  }
+  services.ollama = {
+    enable = true;
+    package = pkgs.unstable.ollama;
+    acceleration = "rocm";
+    sandbox = false;
+    models = "/data/AI/LLMs/Ollama/Models/";
+    environmentVariables = {
+      HSA_OVERRIDE_GFX_VERSION = "10.3.0"; # 680M.
+    };
+  };
+
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+    "armv6l-linux"
+    "armv7l-linux"
+    "i686-linux"
+  ];
+
+  services = {
+    udev = {
+      packages = with pkgs; [ gnome.gnome-settings-daemon ];
+      extraRules = ''
+        SUBSYSTEM=="power_supply", KERNEL=="ADP1", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl --no-block start ac.target"
+
+        # Workstation - keyboard & mouse suspension.
+        ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="024f", ATTR{power/autosuspend}="-1"
+        ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="1bcf", ATTR{idProduct}=="0005", ATTR{power/autosuspend}="-1"
+
+        # KVM switch target.
+        SUBSYSTEM=="usb", ACTION=="add|change|remove", ATTR{idVendor}=="1bcf", ATTR{idProduct}=="0005",  SYMLINK+="kvm-active", TAG+="systemd"
+
+        # Rename network interface.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{DEVTYPE}=="wlan", KERNEL=="wlan*", NAME="wlan0"
+
+        # My personal iPhone.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="12a8", KERNEL=="eth*", NAME="iphone0"
+
+        # My personal OP6T.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="9024", KERNEL=="usb*", NAME="android0"
+
+      '';
+    };
+
+    zfs = {
+      trim = {
+        enable = true;
+        interval = "Sat *-*-* 04:00:00";
+      };
+      autoScrub = {
+        enable = true;
+        interval = "Sat *-*-* 05:00:00";
+      };
+      autoSnapshot.enable = true;
+    };
+    xserver = {
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
+    };
+    auto-cpufreq.enable = false;
+    power-profiles-daemon.enable = pkgs.lib.mkForce false;
+    thermald.enable = true;
+  };
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "performance";
+  };
+
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = false;
+    package = pkgs.steam.override {
+      extraPkgs =
+        pkgs: with pkgs; [
+          #          deckcheatz
+          protontricks
+          protonup-qt
+          python3Full
+          python3Packages.pip
+          python3Packages.virtualenv
+          steamcmd
+          steamtinkerlaunch
+          wemod-launcher
+          wineWowPackages.stable
+          winetricks
+        ];
+    };
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  security = {
+    pam.loginLimits = [
+      {
+        domain = "*";
+        item = "nofile";
+        type = "-";
+        value = "524288";
+      }
+      {
+        domain = "*";
+        item = "memlock";
+        type = "-";
+        value = "524288";
+      }
+    ];
+  };
+  system.stateVersion = "24.05";
+}
